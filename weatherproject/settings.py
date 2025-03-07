b8f2bb94bc880c158085ec478ec3e6951eaa2188
HariPrasath501 <HariPrasath501502@gamil.com>
2025-03-07 13:58:46 +0530

    feat:added extra files

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l!@urdoxk^1n_y4i7_g&%lsce^y1+2j$!dgv=1hz*%n!h61q9_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
